09e1b73507affeb5fae4c5c499d874e76e5e822e
seyyednavid hejazijouybari <seyyednavidhejazijouybari@gmail.com>
2023-04-24 22:32:56 +0100

    Add live search level200

--- script.js
+++ script.js
@@ -1,12 +1,73 @@
-//You can edit ALL of the code here
+//variables
+
+const rootElem = document.getElementById("root"),
+  showEpisodseRow = document.querySelector(".row"),
+  // Search input
+  searchUser = document.querySelector("#searchUser"),
+  // Show the number of episodes based on search
+  searchNumResult = document.querySelector(".epi-num"),
+  // Get an array of objects including all epiodes from episodes.js
+  allEpisodes = getAllEpisodes();
+
+//eventListeners
+eventlisteners();
+function eventlisteners() {
+  // display episodes on load
+  document.addEventListener("DOMContentLoaded", setup);
+  // Search episodes
+  searchUser.addEventListener("keyup", searchMovie);
+}
+
+// get all episodes
 function setup() {
-  const allEpisodes = getAllEpisodes();
+  // call the makeForEpisode function to built card for each of episodes
   makePageForEpisodes(allEpisodes);
 }
 
+//make card for each episode
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  let output = "";
+  episodeList.forEach((episode) => {
+    output += `
+      <div class="col">
+        <div class="card h-100 px-0 mx-1">
+        <img src="${episode.image.medium}" class="card-img-top mx-0" alt="...">
+        <div class="card-body mb-4">
+        <h5 >${episode.name} - S${
+      episode.season < 10 ? "0" + episode.season : episode.season
+    }E${episode.number < 10 ? "0" + episode.number : episode.number}</h5>
+          <p >${episode.summary}</p>
+        </div>
+        <a href="${
+          episode.url
+        }" class="btn btn-primary" style="position:absolute; bottom:10px; left:20px; right:20px;">See More</a>
+      </div>
+      </div>
+    `;
+    showEpisodseRow.innerHTML = output;
+  });
 }
 
-window.onload = setup;
+// show episodes based on live search
+function searchMovie(e) {
+  const selectedMovie = [];
+  // Get the value of input
+  let serchContent = e.target.value;
+  if (serchContent == "") {
+    setup();
+    searchNumResult.textContent = "";
+  } else {
+    // Create case-insensitive RegExp
+    let searchContentInsensitive = new RegExp(serchContent, "i");
+    allEpisodes.forEach((episode) => {
+      if (
+        episode.name.match(searchContentInsensitive) !== null ||
+        episode.summary.match(searchContentInsensitive) !== null
+      ) {
+        selectedMovie.push(episode);
+      }
+    });
+    makePageForEpisodes(selectedMovie);
+    searchNumResult.textContent = ` ${selectedMovie.length}/${allEpisodes.length} `;
+  }
+}
